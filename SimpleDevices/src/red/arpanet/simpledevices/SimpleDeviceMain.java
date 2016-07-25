023ecd6483b5706a82e1b87281df565f3f6e6db9
noworld <noworld@gmail.com>
2016-07-25 14:02:10 -0400

    Fixing typo.

--- SimpleDevices/src/red/arpanet/simpledevices/SimpleDeviceMain.java
+++ SimpleDevices/src/red/arpanet/simpledevices/SimpleDeviceMain.java
@@ -1,6 +1,6 @@
 package red.arpanet.simpledevices;
 
-public class SimpleeDeviceMain {
+public class SimpleDeviceMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
